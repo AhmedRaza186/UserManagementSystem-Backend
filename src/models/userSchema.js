e1d534d2dec2a1a77da1e59cd46646b376ed72e1
AhmedRaza186 <sallumoon650@gmail.com>
2026-04-16 20:02:15 +0500

    ..

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -36,7 +36,7 @@ const UserSchema = new mongoose.Schema({
     },
     isVerified: {
         type: Boolean,
-        default: true
+        default: false
     },
     createdAt: {
         type: Date,
